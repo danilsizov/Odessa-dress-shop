a18da487a0dd5371c081050a1971b4e863062000
Igor Onischenko <optimum.flaminis@gmail.com>
2016-09-10 21:47:07 +0300

    starting product

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-	define("DB_NAME", "odessa_shop_fake");
-	define("DB_USER", "developer");
-	define("DB_PASS", "passvord");
-?>
--- lib/models/product.php
+++ lib/models/product.php
@@ -3,10 +3,19 @@
 
 	class Product
 	{
-		private $db;
+		#private $db;
+		
+		private $id;
+		private $name;
+		private $description;
+		private $old_price;
+		private $new_price;
+		private $samples;
+		private $rate;
+
 		public function __construct()
 		{
-			$this->db = Database::getInstance();
+			#$this->db = Database::getInstance();
 		}
 		public static function getById($id){
 			#$db_result = $this->db->query("SELECT * FROM products WHERE id = '{$id}'");
@@ -19,9 +28,56 @@
 			$db = Database::getInstance();
 			return $db->query("SELECT COUNT(*) as count FROM products")->fetch_assoc()['count'];
 		}
+		public function getId(){
+			return $this->id;
+		}
+		public function getName(){
+			return $this->name;
+		}
+		public function getDescription(){
+			return $this->description;
+		}
+		public function getOldPrice(){
+			return $this->old_price;
+		}
+		public function getNewPrice(){
+			return $this->new_price;
+		}
+		public function getSamples(){
+			return $this->samples;
+		}
 		public function getRate(){
+			return $this->rate;
+		}
+		public function setId(){
 			
 		}
-
+		public function setName(){
+			
+		}
+		public function setDescription(){
+			
+		}
+		public function setOldPrice(){
+			
+		}
+		public function setNewPrice(){
+			
+		}
+		public function setSamples(){
+			
+		}
+		public function setRate(){
+			
+		}
+		public function toArray(){
+			$ar = array('id' => $this->getId(),
+						'name' => $this->getName(),
+						'description' => $this->getDescription(),
+						'old_price' => $this->getOldPrice(),
+						'new_price' => $this->getNewPrice(),
+						'sample' => $this->getSample(),
+						'rate' => $this->getRate());
+		}
 	}
 ?>
